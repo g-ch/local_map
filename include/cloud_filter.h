172adb59f9447bda1da0a06858b13c14dc7af794
g-ch <947089399@qq.com>
2018-05-20 13:20:25 +0800

    visualization

--- include/cloud_filter.h
+++ include/cloud_filter.h
@@ -5,14 +5,54 @@
 #ifndef LOCAL_MAP_CLOUD_FILTER_H
 #define LOCAL_MAP_CLOUD_FILTER_H
 
-#include "ros/ros.h"
-#include <string.h>
+#include <string>
 #include <iostream>
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/statistical_outlier_removal.h>
-#include "sensor_msgs/image_encodings.h"
+#include <pcl/visualization/pcl_visualizer.h>
 
+class CloudProcess
+{
+public:
+    CloudProcess();
+    ~CloudProcess();
+
+    /**
+     * Main process function
+     */
+    void process();
+
+    /**
+     * Display function
+     */
+    void viewPointXYZPtr(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud_ptr);
+    void viewPointXYZ(pcl::PointCloud<pcl::PointXYZ> cloud);
+
+    /**
+     * Global mat for cloud
+     */
+    pcl::PointCloud<pcl::PointXYZ> input_cloud;
+    pcl::PointCloud<pcl::PointXYZ> output_cloud;
+
+    /**
+     * Cloud viewer
+     */
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> cloud_viewer;
+
+    /**
+     * Paremeters for filters
+     */
+    struct Statistical_Filter
+    {
+        bool Use;
+        int Mean_K;
+        double Stddev_Mul_Thresh;
+    }sf;
+
+private:
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> PointXYZViewer(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud);
+};
 
 #endif //LOCAL_MAP_CLOUD_FILTER_H
--- src/cloud_filter.cpp
+++ src/cloud_filter.cpp
@@ -4,63 +4,65 @@
 
 #include "../include/cloud_filter.h"
 
-using namespace std;
 
-// global mat for cloud
-pcl::PointCloud<pcl::PointXYZ> input_cloud;
-pcl::PointCloud<pcl::PointXYZ> output_cloud;
-pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_ptr( new pcl::PointCloud<pcl::PointXYZ>);
 
-sensor_msgs::PointCloud2 cloud2;
-ros::Publisher cloud_pub;
-
-
-void cloudCallback(const sensor_msgs::PointCloud2ConstPtr &msg)
+CloudProcess::CloudProcess()
 {
-    /*Update data*/
-    cout << "new cloud " << endl;
-    input_cloud.points.clear();
-    pcl::fromROSMsg(*msg, input_cloud);
-
-    /*Link*/
-    input_cloud_ptr = input_cloud.makeShared();
-    output_cloud_ptr = output_cloud.makeShared();
-
-    /*Filter process*/
-    // Create the filtering object
-    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-    sor.setInputCloud (input_cloud_ptr);
-    sor.setMeanK (20);
-    sor.setStddevMulThresh (0.05);
-    sor.filter (*output_cloud_ptr);
-
-    /*Publish*/
-    output_cloud = input_cloud;
-    pcl::toROSMsg(output_cloud, cloud2);
-    cloud2.header.frame_id = "world";
-    cloud_pub.publish(cloud2);
-    cout << "cloud publish to rviz! \n" << endl;
+    sf.Use = true;
+    sf.Mean_K = 20;
+    sf.Stddev_Mul_Thresh = 0.1;
 }
 
-
-/* @function main */
-int main( int argc, char **argv )
+CloudProcess::~CloudProcess()
 {
-    ros::init(argc, argv, "cloud_filter");
-    ros::NodeHandle n;
 
-    ros::Subscriber cloud_sub = n.subscribe("/ring_buffer/cloud2", 1, cloudCallback);
-    cloud_pub = n.advertise<sensor_msgs::PointCloud2>("/local_map/filtered_cloud", 2);
+}
 
-    ros::Rate loop_rate(10);
+void CloudProcess::process()
+{
+    std::cout<<"start process"<<std::endl;
 
-    /*Main loop*/
-    while(ros::ok())
+    input_cloud_ptr = input_cloud.makeShared(); //ISSUE
+
+    /* Create the filtering object */
+    // Statistical Filter
+    if(sf.Use)
     {
-        ros::spinOnce();
-        loop_rate.sleep();
+        pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+        sor.setInputCloud (input_cloud_ptr);
+        sor.setMeanK (sf.Mean_K);
+        sor.setStddevMulThresh (sf.Stddev_Mul_Thresh);
+        sor.filter (output_cloud);
     }
 
-    return 0;
+
+    std::cout<<"Process finished"<<endl;
+    std::cout<<output_cloud<<endl;
+}
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> CloudProcess::PointXYZViewer(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
+{
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->setBackgroundColor (0, 0, 0);
+
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color (cloud, 0, 255, 0); //add green color
+    viewer->addPointCloud<pcl::PointXYZ> (cloud, single_color, "cloud");
+
+    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
+    viewer->addCoordinateSystem(1.0);
+    viewer->initCameraParameters();
+    return (viewer);
+}
+
+void CloudProcess::viewPointXYZPtr(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud_ptr)
+{
+    cloud_viewer = PointXYZViewer(cloud_ptr);
+}
+
+void CloudProcess::viewPointXYZ(pcl::PointCloud<pcl::PointXYZ> cloud)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr( new pcl::PointCloud<pcl::PointXYZ>);
+    cloud_ptr = cloud.makeShared();
+    cloud_viewer = PointXYZViewer(cloud_ptr);
 }
